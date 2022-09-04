a989a3e89e0eb054117f056935dae73519244abf
Rosalie Newman <rosalienewman0@gmail.com>
2022-09-03 22:16:55 -0400

    Clicking the dark/light mode icon will toggle the icon as well as BG color for sections and page title. Does not change the font color for the paragraphs in About and Application.

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ $(document).ready(function () {
 });
 
 // Dark / Light mode toggling
+const colormodeDiv = document.getElementById('colormode-div');
+const allSections = document.getElementsByTagName('section');
+const pageTitle = document.getElementsByTagName('h1')[0];
+
+colormodeDiv.addEventListener('click', (event) => {
+	const elementsToGetDarkBackground = [...allSections];
+	elementsToGetDarkBackground.push(pageTitle);
+
+	elementsToGetDarkBackground.forEach((elem) => {
+		elem.classList.toggle('darkMode');
+	});
+
+	// Toggle the icon
+	if (event.target.src.includes('sun')) {
+		event.target.src = './images/moon.svg';
+	} else {
+		event.target.src = './images/sun.svg';
+	}
+});
 
 // Application CLOSED alert
 const applicationButton = document.getElementById('application-button');
